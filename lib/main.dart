a3162ad62e2cc0f5af4cc822144d775816649038
eng-batol <batol.alashwak@gmail.com>
2024-10-23 09:13:15 +0300

    pet

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 // import 'package:adopt_app/pages/.dart';
-import 'package:adopt_app/pages/update_page.dart'; // UpdatePage is properly imported
+import 'package:adopt_app/pages/update_page.dart'; 
 import 'package:adopt_app/pages/home_page.dart';
 // import 'package:adopt_app/pages/updatePage.dart';
 import 'package:adopt_app/providers/pets_provider.dart';
@@ -7,7 +7,7 @@ import 'package:adopt_app/widgets/add_form.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
-import 'package:adopt_app/pages/add_page.dart'; // Correctly pointing to the file in the 'pages' folder
+import 'package:adopt_app/pages/add_page.dart';
 
 void main() {
   runApp(
@@ -56,7 +56,7 @@ class MyApp extends StatelessWidget {
           final pet = Provider.of<PetsProvider>(context).pets.firstWhere(
               (pet) =>
                   pet.id.toString() ==
-                  state.pathParameters['petId']!); // Find the pet by ID
+                  state.pathParameters['petId']!); 
           return UpdatePage(pet: pet);
         },
       ),
